87e3012e6cb771417c12eaa1945b5cdcf6f15a6d
mdmasudrana5060 <ranam5060@gmail.com>
2023-01-31 12:08:45 +0600

    vercel added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
@@ -9,6 +10,25 @@ const port = process.env.PORT || 5000
 // middle ware
 app.use(cors());
 app.use(express.json());
+
+function verifyJwt(req, res, next) {
+    const authHeader = req.headers.authorization;
+    console.log('inside verifyjwt function', authHeader);
+    if (!authHeader) {
+        return res.sendStatus(401).send({ message: "unauthorized access" })
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.sendStatus(403).send({ message: ' forbidden' })
+        }
+        req.decoded = decoded;
+
+        next()
+    })
+
+
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2cacwdw.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -23,6 +43,17 @@ async function run() {
         const orderCollection = database.collection('orders');
 
 
+        // auth
+
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            })
+            res.send({ accessToken });
+        })
+
+
         // get all products
         app.get('/products', async (req, res) => {
 
@@ -30,8 +61,8 @@ async function run() {
             const size = parseInt(req.query.size);
             const query = {};
             const cursor = productCollection.find(query);
-            const count = await cursor.count();
-
+            // const count = await productCollection.count();
+            const count = await productCollection.estimatedDocumentCount();
             let products;
 
             if (page) {
@@ -65,6 +96,9 @@ async function run() {
             res.send(products)
 
         })
+
+
+        //post order
         app.post('/orders', async (req, res) => {
             const order = req.body;
             console.log(req.body);
@@ -72,6 +106,30 @@ async function run() {
             res.json(result);
 
         })
+        // get orders
+        app.get('/orders', verifyJwt, async (req, res) => {
+            const email = req.query.email;
+            const decodedEmail = req.decoded.email;
+            console.log(decodedEmail);
+            if (decodedEmail === email) {
+                const query = { email };
+                const cursor = orderCollection.find(query);
+                const order = await cursor.toArray();
+                res.send(order);
+
+            }
+            else {
+                res.sendStatus(403)
+            }
+
+
+
+
+
+        })
+
+
+
 
 
     }
